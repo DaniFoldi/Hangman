c39a3c77df6369aee1e1294c1108470927f2d33b
DaniFoldi <dani@stefan.hu>
2017-07-02 15:11:33 +0200

    Cleanup

--- main.py
+++ main.py
@@ -500,15 +500,6 @@ class color:
   bold = "\033[1m"
   underline = "\033[4m"
   end = "\033[0m"
-#class color:
-#    HEADER = '\033[95m'
-#    OKBLUE = '\033[94m'
-#    OKGREEN = '\033[92m'
-#    WARNING = '\033[93m'
-#    FAIL = '\033[91m'
-#    ENDC = '\033[0m'
-#    BOLD = '\033[1m'
-#    UNDERLINE = '\033[4m'
 
 #HANGMANIMATION
 
